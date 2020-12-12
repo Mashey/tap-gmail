6533686e97901d37c801bcf60bdac25a058ceb6d
Jordan <jordan.wallace.williams@gmail.com>
2020-12-12 12:35:04 -0700

    Remove GCP Secret Manager functions and package

--- tap_gmail/reports_service.py
+++ tap_gmail/reports_service.py
@@ -1,26 +1,9 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from google.auth.transport.requests import AuthorizedSession
-from google.cloud import secretmanager
 import json
 import os
 
-
-def access_secret_version(project_id, secret_id, version_id):
-    # Create the Secret Manager client.
-    secret_client = secretmanager.SecretManagerServiceClient()
-
-    # Build the resource name of the secret version.
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-
-    # Access the secret version.
-    response = secret_client.access_secret_version(request={"name": name})
-    data = json.loads(response.payload.data)
-
-    return data
-
-
-# SERVICE_ACCOUNT_FILE = access_secret_version('102430623331', 'service_key', 1)
 SERVICE_ACCOUNT_FILE = os.getenv("ADMIN_SDK_KEY")
 SERVICE_ACCOUNT_FILE_JSON = json.loads(SERVICE_ACCOUNT_FILE)
 
